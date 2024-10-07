6d154875c8d13afec652859c51fbf7374a5a7199
程序员一棵树 <420137138@qq.com>
2024-10-08 01:00:48 +0800

    Feature：初始化。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blogs/",
+  base: "/blog/",
 
   lang: "zh-CN",
   title: "程序员一棵树",
